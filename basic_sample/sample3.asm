9c0b250d09cd27f59b891b6d1911d628532fae24
Hirofumi Seiyama <hseiyama@gmail.com>
2023-04-12 23:49:28 +0900

    基本サンプルを追加

---
+++ basic_sample/sample3.asm
@@ -0,0 +1,19 @@
+;***********************************************************
+;SAMPLE3
+;***********************************************************
+;
+GUSUR	EQU	0200h
+KISUR	EQU	025Fh
+	ORG	0000h
+START:
+	LD	B,0		;B <- 0
+LOOP2:
+	SRL	A		;A <- A<<1
+	JR	NC,JP1
+	INC	B		;B <- B+1
+JP1:
+	JR	NZ,LOOP2
+	BIT	0,B
+	JP	Z,GUSUR
+	JP	KISUR
+	END
